158c5d0c47970e04cfcbbb6678cbb11856cc9a6d
MouradTgit <mourad.toure@students.dominican.edu>
2026-02-19 10:36:19 -0800

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Title.js'
+import POPOSList from './POPOSList';
 
 function App() {
   return (
     <div className="App">
+      <Title />
+      <POPOSList />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
